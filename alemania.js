87911e6713385978c14a650055272dc3da88eae7
Mateoschaller <125513010+Mateoschaller@users.noreply.github.com>
2023-03-18 13:39:00 -0300

    2.0

---
+++ alemania.js
@@ -0,0 +1,119 @@
+const ingl = document.querySelector(".ingl")
+const alem = document.querySelector(".alem")
+const espa = document.querySelector(".espa")
+const fran = document.querySelector(".fran")
+const ital = document.querySelector(".ital")
+const arge = document.querySelector(".arge")
+const bun1 = document.querySelector(".bun1")
+const bun2 = document.querySelector(".bun2")
+const copaal = document.querySelector(".copaal")
+const superal = document.querySelector(".superal")
+const copalal = document.querySelector(".copalal")
+const aus = document.querySelector(".aus")
+const dor = document.querySelector(".dor")
+const lev = document.querySelector(".lev")
+const bayer = document.querySelector(".bayer")
+const boc = document.querySelector(".boc")
+const col = document.querySelector(".col")
+const frank = document.querySelector(".frank")
+const fri = document.querySelector(".fri")
+const hert = document.querySelector(".hert")
+const hoff = document.querySelector(".hoff")
+const main = document.querySelector(".main")
+const monc = document.querySelector(".monc")
+const lei = document.querySelector(".lei")
+const scha = document.querySelector(".scha")
+const stt = document.querySelector(".stt")
+const uni = document.querySelector(".uni")
+const brem = document.querySelector(".brem")
+const wolf = document.querySelector(".wolf")
+
+
+
+ingl.addEventListener("click", () => {
+    window.open("inglaterra.html")
+})
+alem.addEventListener("click", () => {
+    window.open("alemania.html")
+})
+espa.addEventListener("click", () => {
+    window.open("españa.html")
+})
+fran.addEventListener("click", () => {
+    window.open("francia.html")
+})
+ital.addEventListener("click", () => {
+    window.open("italia.html")
+})
+arge.addEventListener("click", () => {
+    window.open("")
+})
+bun1.addEventListener("click", () => {
+    window.open("bundesliga.html")
+})
+bun2.addEventListener("click", () => {
+    window.open("bundesliga2.html")
+})
+copaal.addEventListener("click", () => {
+    window.open("copaalemania.html")
+})
+superal.addEventListener("click", () => {
+    window.open("supercopaalemana.html")
+})
+copalal.addEventListener("click", () => {
+    window.open("copadelalligaalemana.html")
+})
+aus.addEventListener("click", () => {
+    window.open("ausburgo.html")
+})
+dor.addEventListener("click", () => {
+        window.open("dortmund.html")
+    })
+lev.addEventListener("click", () => {
+        window.open("leverkusen.html")
+    })
+bayer.addEventListener("click", () => {
+        window.open("bayermunich.html")
+    })
+boc.addEventListener("click", () => {
+        window.open("bochum.html")
+    })
+col.addEventListener("click", () => {
+        window.open("colonia.html")
+    })
+frank.addEventListener("click", () => {
+        window.open("frankfurt.html")
+    })
+fri.addEventListener("click", () => {
+        window.open("friburgo.html")
+    })
+hert.addEventListener("click", () => {
+        window.open("hertaberlin.html")
+    })
+hoff.addEventListener("click", () => {
+        window.open("hoffenheim.html")
+    })
+main.addEventListener("click", () => {
+        window.open("mainz.html")
+    })
+monc.addEventListener("click", () => {
+        window.open("Monchengladbach.html")
+    })
+lei.addEventListener("click", () => {
+        window.open("leipzigg.html")
+    })
+scha.addEventListener("click", () => {
+        window.open("schalke.html")
+    })
+stt.addEventListener("click", () => {
+        window.open("stuttgart.html")
+    })
+uni.addEventListener("click", () => {
+        window.open("unionberlin.html")
+    })
+brem.addEventListener("click", () => {
+        window.open("werderbremen.html")
+    })
+wolf.addEventListener("click", () => {
+        window.open("wolfsburgo.html")
+    })
